9d8ffb577ed587048f1cc57a6bf1ca96a26f96ab
Nick Davis <nick.davis@firstlinesoftware.com>
2023-03-22 17:56:25 +0300

    output

--- main.tf
+++ main.tf
@@ -49,4 +49,8 @@ resource "yandex_compute_instance" "vm-1" {
       private_key = "${file("~/.ssh/id_rsa")}"
     }
   }
+
+  output "external_ip_address_vm_1" {
+    value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+  }
 }
